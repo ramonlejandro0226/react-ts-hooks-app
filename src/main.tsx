6bf1922dd59bc4ed88e0d5003f1abf392e03cdbe
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-20 14:18:05 -0400

    feat: add TrafficLightWithEffect component with countdown and color change effects

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import { HooksApp } from './HooksApp';
-import { TrafficLight } from './01-useState/TrafficLight';
+// import { TrafficLight } from './01-useState/TrafficLight';
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    <TrafficLightWithEffect />
   </StrictMode>
 );
