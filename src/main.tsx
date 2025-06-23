cebacafe1c8c5ad80ee4a4e99b699a12b3f121e5
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-23 08:57:30 -0400

    feat: implement TrafficLightWithHook component with countdown and color change logic

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 // import { HooksApp } from './HooksApp';
 // import { TrafficLight } from './01-useState/TrafficLight';
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 
 import './index.css';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
-    <TrafficLightWithEffect />
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
   </StrictMode>
 );
